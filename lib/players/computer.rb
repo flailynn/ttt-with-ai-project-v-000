432f9102c8f1c4b34e515c1e87195fd01c9ea756
Anita Beauchamp <amadamspr@gmail.com>
2018-09-04 02:09:16 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,7 @@ module Players
     FIRST_TURN_STRATEGIES = ["1", "3","5","7", "9"]
     EDGE_LOCATIONS = ["2","4","6","8"]
     CORNER_LOCATIONS = ["1", "3", "7", "9"]
+    attr_reader :strategy
 
     def move(board)
       if board.turn_count == 0

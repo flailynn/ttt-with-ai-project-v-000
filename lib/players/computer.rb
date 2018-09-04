9efe8c24dc12b3845755629142f87375fadc37ec
Anita Beauchamp <amadamspr@gmail.com>
2018-09-04 02:07:26 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 module Players
   class Computer < Player
-    FIRST_TURN_STRATEGIES = ["2","4","5","6","8"]
+    FIRST_TURN_STRATEGIES = ["1", "3","5","7", "9"]
     EDGE_LOCATIONS = ["2","4","6","8"]
     CORNER_LOCATIONS = ["1", "3", "7", "9"]
 
